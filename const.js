f6567d79757f0ae073c70a55dd82ff0c7db184da
jammyjamsticy <satishtripathy11@gmail.com>
2016-11-23 17:45:57 +0530

    Update const.js

--- const.js
+++ const.js
@@ -1,15 +1,15 @@
 'use strict';
 
 // Wit.ai parameters
-const WIT_TOKEN = process.env.WIT_TOKEN;
+const WIT_TOKEN = process.env.WIT_TOKEN || 'TYG2VE2HRJIV6BNA5LD2L2XJO4V5JJSQ';
 if (!WIT_TOKEN) {
   throw new Error('missing WIT_TOKEN');
 }
 
 // Messenger API parameters
-const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN;
+const FB_PAGE_TOKEN = process.env.FB_PAGE_TOKEN || 'EAAGGQ9ZAaQDQBADZCghP5YINwUgYwxBdmVxFKMsg6ewkGVBT5ZC1rfJO3ZCxX5eYkBa1PWiV197cnNvqXuCOQ1ot1eu8q3dsvecsfPKLQBms4LTEMcO1t0q8qrmksJjwBLrnmhxJlCswOF5RXa9vnHYs73M4bvlm9jHUPmmCyAZDZD';
 
-var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
+var FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN || 'just_do_it';
 if (!FB_VERIFY_TOKEN) {
   FB_VERIFY_TOKEN = "just_do_it";
 }
